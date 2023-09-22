1485d0b28932e5d3622db1acbc64c4d03e52a349
Aj-1872 <ajayjodedra@gmail.com>
2023-09-21 20:39:24 -0700

    adding comments

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,22 @@
+// Import the Mongoose library
 const mongoose = require('mongoose');
+
+// Define the MongoDB URI (Uniform Resource Identifier) where your database is located
 const mongoURI = "mongodb://127.0.0.1:27017/NoteBook";
 
-
+// Create an asynchronous function to connect to MongoDB
 const connectToMongo = async () => {
     try {
+        // Use the `mongoose.connect` method to connect to the MongoDB database
         await mongoose.connect(mongoURI);
+
+        // If the connection is successful, log a success message
         console.log("Connected to MongoDB successfully");
     } catch (error) {
+        // If there's an error while connecting, log the error message
         console.error("Error connecting to MongoDB:", error);
     }
 };
 
+// Export the `connectToMongo` function so it can be used in other parts of your application
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
+// Import the `connectToMongo` function from the './db' module
 const connectToMongo = require('./db');
-const express = require('express')
 
+// Import the 'express' library to create an Express.js application
+const express = require('express');
+
+// Call the `connectToMongo` function to establish a connection to MongoDB
 connectToMongo();
-const app = express()
-const port = 5000
 
-app.use(express.json())
+// Create an instance of an Express.js application
+const app = express();
 
+// Define the port where the Express.js server will listen for incoming requests
+const port = 5000;
+
+// Use JSON middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Define routes for your application
+
+// Example route that responds with 'Hello World!'
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// Define routes for authentication and notes API using external modules
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
-
+// Start the Express.js server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
